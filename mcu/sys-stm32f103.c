80a94972a93528d41413f103d70a62de6eff8c96
Samuel <tuglersamuel@gmail.com>
2020-05-18 22:21:24 +0200

    Augmented blink speed

--- mcu/sys-stm32f103.c
+++ mcu/sys-stm32f103.c
@@ -65,11 +65,14 @@ static void wait (int count)
 
 void wait_button() {
     #if defined(GPIO_BUTTON_PIN)
-        while (GPIO_OTHER->IDR & (1 << GPIO_BUTTON_PIN)){
+        while (true){
             set_led(1);
-            wait(1000000);
+            wait(700000);
+            if((GPIO_OTHER->IDR & (1 << GPIO_BUTTON_PIN)) == 1) break;
+
             set_led(0);
-            wait(1000000);
+            wait(700000);
+            if((GPIO_OTHER->IDR & (1 << GPIO_BUTTON_PIN)) == 1) break;
         }
     #endif
 }
