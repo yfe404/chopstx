f6484c0ed6b72c40f9b412e98d26457f92f013f5
Samuel <tuglersamuel@gmail.com>
2020-05-18 19:23:56 +0200

    Added turtle-auth board Added wait_button function for stm32f103

---
+++ board/board-turtle-auth.h
@@ -0,0 +1,38 @@
+#define BOARD_NAME "Turtle Auth"
+/* echo -n "Turtle Auth" | shasum -a 256 | sed -e 's/^.*\(........\)  -$/\1/' */
+#define BOARD_ID    0xcff5fffd
+
+#define MCU_STM32F1 1
+#define STM32F10X_MD		/* Medium-density device */
+
+#define STM32_PLLXTPRE                  STM32_PLLXTPRE_DIV1
+#define STM32_PLLMUL_VALUE              9
+#define STM32_HSECLK                    8000000
+
+#define GPIO_LED_BASE           GPIOC_BASE
+#define GPIO_LED_SET_TO_EMIT      13
+#define GPIO_USB_BASE           GPIOA_BASE
+#define GPIO_OTHER_BASE         GPIOA_BASE
+#define GPIO_BUTTON_PIN			  8
+
+/*
+ * Port A setup.
+ * PA11 - Push Pull output 10MHz 0 default (until USB enabled) (USBDM)
+ * PA12 - Push Pull output 10MHz 0 default (until USB enabled) (USBDP)
+ *
+ * Port C setup.
+ * PC13 - Push pull output 50MHz (LED 1:ON 0:OFF)
+ * ------------------------ Default
+ * PAx  - input with pull-up
+ * PCx  - input with pull-up
+ */
+#define VAL_GPIO_USB_ODR            0xFFFFE6FF
+#define VAL_GPIO_USB_CRL            0x88888888      /*  PA7...PA0 */
+#define VAL_GPIO_USB_CRH            0x88811888      /* PA15...PA8 */
+
+#define VAL_GPIO_LED_ODR            0xFFFFFFFF
+#define VAL_GPIO_LED_CRL            0x88888888      /*  PC7...PC0 */
+#define VAL_GPIO_LED_CRH            0x88388888      /* PC15...PC8 */
+
+#define RCC_ENR_IOP_EN      (RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN)
+#define RCC_RSTR_IOP_RST    (RCC_APB2RSTR_IOPARST | RCC_APB2RSTR_IOPCRST)
--- mcu/sys-stm32f103.c
+++ mcu/sys-stm32f103.c
@@ -63,6 +63,16 @@ static void wait (int count)
     asm volatile ("" : : "r" (i) : "memory");
 }
 
+void wait_button() {
+    #if defined(GPIO_BUTTON_PIN)
+        while (GPIO_OTHER->IDR & (1 << GPIO_BUTTON_PIN)){
+            set_led(1);
+            wait(1000000);
+            set_led(0);
+            wait(1000000);
+        }
+    #endif
+}
 
 static void
 usb_lld_sys_shutdown (void)
